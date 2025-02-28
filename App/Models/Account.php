78742132578c0930eab7dea00335008996d57998
jhoel0521 <jhoel0521@gmail.com>
2025-02-28 12:09:39 -0400

    Eliminar comentarios de la definición de la tabla Account en el modelo

--- App/Models/Account.php
+++ App/Models/Account.php
@@ -7,16 +7,6 @@ use Core\Model;
 class Account extends Model
 {
 
-    // id INT AUTO_INCREMENT PRIMARY KEY,
-    // currentBalance DECIMAL(10, 2) DEFAULT 0.00,
-    // type ENUM ('CA', 'CC') NOT NULL COMMENT 'CA: Current Account, CC: Savings Account',
-    // status ENUM ('AC', 'IN') DEFAULT 'AC' COMMENT 'AC: Active, IN: Inactive',
-    // personId INT NOT NULL,
-    // officeId INT NOT NULL,
-    // created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    // updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    // FOREIGN KEY (personId) REFERENCES Person (id) ON DELETE CASCADE,
-    // FOREIGN KEY (officeId) REFERENCES Office (id) ON DELETE CASCADE
     protected $table = 'Account';
     protected $primaryKey = 'id';
     protected $fillable = ['currentBalance', 'type', 'status', 'personId', 'officeId'];
