04efe7e9cdfea9529652ee7ebefb701b41fc7471
Jhoel <jhoel0521@gmail.com>
2025-03-02 22:38:14 -0400

    Actualizar el favicon de la plantilla de la aplicación

--- resources/views/layouts/app.view.php
+++ resources/views/layouts/app.view.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BancoFassil - <?=$title??traducir('title')?></title>
     <!-- ico -->
-    <link rel="icon" href="<?= asset('images/logo-BancoFassil.jpg') ?>" type="image/x-icon">
+    <link rel="icon" href="<?= asset('images/favicon.ico') ?>" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
