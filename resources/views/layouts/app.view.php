f993a006f2de20cbfc736df9765595cb76646a0b
Jhoel <jhoel0521@gmail.com>
2025-03-02 22:32:27 -0400

    Agregar favicon a la plantilla de la aplicación

--- resources/views/layouts/app.view.php
+++ resources/views/layouts/app.view.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BancoFassil - <?=$title??traducir('title')?></title>
+    <!-- ico -->
+    <link rel="icon" href="<?= asset('images/logo-BancoFassil.jpg') ?>" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
