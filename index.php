7004b7b82622253e88cbe644f2dbecb4de527673
Jhoel <jhoel0521@gmail.com>
2025-03-04 15:55:31 -0400

    Eliminar la cabecera de credenciales CORS en index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, Cache-Control');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 
