916b95a108b495a3d9e3a82ca767e174bc54a955
jhoel0521 <jhoel0521@gmail.com>
2025-03-30 20:25:27 -0400

    Actualizar encabezados CORS para permitir el uso de X-Mi-Token

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, Cache-Control');
+header('Access-Control-Allow-Headers: X-Mi-Token, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, Cache-Control');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 
 // Respuesta para la solicitud OPTIONS (preflight)
